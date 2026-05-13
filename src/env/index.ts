0190a559efd94c7280b2f9fda2ced956adfbd7c9
ArthurFloro <arthur.floro2006@gmail.com>
2026-05-12 22:16:11 -0300

    feat: adicionando dotenv e tipagem das variaveis de ambiente

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(3333)
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+    const err = _env.error
+
+    console.error('❌ Invalid environment variables', z.treeifyError(err))
+
+    throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
