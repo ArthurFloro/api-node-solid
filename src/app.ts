16397601fb3713cda079f15219c43418d10c065b
ArthurFloro <arthur.floro2006@gmail.com>
2026-05-15 18:55:49 -0300

    feat: criando o usuário, adaptando o schema e o env

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import z from 'zod'
+import { prisma } from './lib/prisma.js'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const requestBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string(),
+  })
+
+  const { name, email, password } = requestBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
