ed5db7f16b12dd62a2bb58c5be8f4683cc76a75b
ArthurFloro <arthur.floro2006@gmail.com>
2026-06-29 22:25:48 -0300

    fix: correct test script and update register use case

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,18 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register.js'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+  it('should hash passowrd upon registration', async () => {
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Arthur Floro',
+      email: 'arthur@arthur.com',
+      password: '123456',
+    })
+
+    console.log(user.password_hash)
+  })
 })
