fe55c0bed26720cf32a71f58d126aebb509f2afb
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-02 19:51:34 -0300

    feat(users): cria use-case e testes para o perfil do usuário
    - Implementa o caso de uso responsável por recuperar as informações do perfil do usuário logado.
    - Adiciona a suíte de testes unitários para validar se os dados retornados pela aplicação estão corretos.

--- src/use-cases/factories/make-authenticate-use-case.ts
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -1,10 +1,9 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
-
 import { AuthenticateUseCase } from '../authenticate.js'
 
 export function makeAuthenticateUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
 
-  return authenticateUseCase
+    return authenticateUseCase
 }
