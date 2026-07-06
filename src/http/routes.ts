25733240c072e13b06334440b3ed53ecb26f3d91
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-06 19:04:14 -0300

    feat(api): implementa controller de perfil e middleware de autenticação JWT
    - Integra o use-case de perfil de usuário ao controller de perfil para buscar os dados do usuário autenticado.
    - Cria o middleware de autenticação para validar o token JWT recebido nas requisições.
    - Configura o middleware para interceptar requisições não autorizadas e retornar mensagens de erro personalizadas e amigáveis.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,6 +4,7 @@ import { authenticate } from './controllers/authenticate.js'
 import { profile } from './controllers/profile.js'
 import fastifyJwt from '@fastify/jwt'
 import { env } from '@/env/index.js'
+import { verifyJWT } from './middlewares/verify-jwt.js'
 
 export async function appRoutes(app: FastifyInstance) {
   app.register(fastifyJwt, {
@@ -13,5 +14,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
-  app.get('/me', profile)
+  app.get('/me', {onRequest: [verifyJWT]},profile)
 }
