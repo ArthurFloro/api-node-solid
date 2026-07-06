8ac48f404d84d9379566027abe69b2fee4ada056
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-06 18:49:33 -0300

    feat(auth): integra @fastify/jwt para autenticação na aplicação
    - Adiciona a biblioteca  ao projeto.
    - Implementa a infraestrutura para geração e validação de tokens JWT (JSON Web Token).
    - Prepara a base de segurança necessária para proteger as rotas da aplicação.

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,17 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register.js'
 import { authenticate } from './controllers/authenticate.js'
+import { profile } from './controllers/profile.js'
+import fastifyJwt from '@fastify/jwt'
+import { env } from '@/env/index.js'
 
 export async function appRoutes(app: FastifyInstance) {
+  app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+  })
+
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
