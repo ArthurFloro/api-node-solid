7242bc52c1b5391a3a1f2fbcbce155b71c07453a
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-06 20:01:28 -0300

    test(e2e): implementa teste e2e para o controller de autenticação
    - Adiciona teste de ponta a ponta para a rota de criação de sessão (/sessions).
    - Utiliza o pacote Supertest para simular as requisições HTTP na API.
    - Valida se o processo de login ocorre com sucesso e se a aplicação retorna corretamente um token de autenticação em formato de string.

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import { app } from '@/app.js'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Authenticate (e2e', () => {
+    beforeAll(async() => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to authenticate', async () => {
+        await request(app.server)
+        .post('/users')
+        .send({
+            name: 'Arthur Floro', 
+            email: 'arthur@arthur.com', 
+            password: '123456'
+        })
+
+        const response = await request(app.server)
+        .post('/sessions')
+        .send({
+           email: 'arthur@arthur.com', 
+            password: '123456'
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toEqual({
+            token: expect.any(String)
+        })
+    })
+})
