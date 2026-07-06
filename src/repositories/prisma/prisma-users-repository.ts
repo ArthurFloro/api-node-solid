ed03f4b86bc0644805df1eaab9ba3e671d42927a
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-06 12:30:23 -0300

    feat(prisma): implementa repositórios concretos do banco de dados
    Inicia a implementação das classes de repositório utilizando o Prisma Client para realizar as operações no banco de dados, seguindo as interfaces de contrato previamente definidas.

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,8 +3,14 @@ import { Prisma, type User } from 'generated/prisma/client.js'
 import type { UsersRepository } from '../users-repository.js'
 
 export class PrismaUsersRepository implements UsersRepository {
-  findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
   }
 
   async findByEmail(email: string) {
