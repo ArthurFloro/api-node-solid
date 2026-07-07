63e1ac491f963f82b8857112f4ea435eb71590b1
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-07 17:14:50 -0300

    test(e2e): corrige testes após implementação do controle de acesso (RBAC)
    - Ajusta a função de criação e autenticação de usuários nos testes para incluir a role adequada.
    - Garante que as simulações de requisição HTTP enviem as credenciais corretas para passar na nova validação de permissões.
    - Restaura a integridade da suíte de testes e2e que havia quebrado com a nova política de segurança.

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,25 +1,29 @@
+import { prisma } from '@/lib/prisma.js'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-    await request(app.server)
-        .post('/users')
-        .send({
-            name: 'Arthur Floro', 
-            email: 'arthur@arthur.com', 
-            password: '123456'
-        })
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
 
-        const authResponse = await request(app.server)
+    await prisma.user.create({
+        data: {
+            name: 'Arthur floro',
+            email: 'arthur@arthur.com',
+            password_hash: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER'
+        }
+    })
+
+    const authResponse = await request(app.server)
         .post('/sessions')
         .send({
-           email: 'arthur@arthur.com', 
+            email: 'arthur@arthur.com',
             password: '123456'
         })
 
-        const {token} = authResponse.body
+    const { token } = authResponse.body
 
-        return {
-            token
-        }
+    return {
+        token
+    }
 }
